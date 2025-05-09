1b249dddd3cb8c4ff10a15db396e2108fccd1852
Lucas Yamada <{email}>
2025-05-09 03:16:02 -0300

    dev phase 3.2

--- src/components/Produtos.jsx
+++ src/components/Produtos.jsx
@@ -12,7 +12,7 @@ function Produtos() {
 
   const getProdutos = async () => {
     try {
-      const response = await fetch("http://localhost:3000/produtos");
+      const response = await fetch("https://loja-online-back-6j5q.onrender.com/produtos");
       const data = await response.json();
       console.log(data);
 
